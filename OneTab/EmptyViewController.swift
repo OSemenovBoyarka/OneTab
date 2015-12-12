a8e1b843816e10129252806a38c8a81893cfc829
Alexander Semenov <asemenovboyarka@gmail.com>
2015-12-12 18:04:26 +0200

    added greeting after close last page

--- OneTab/EmptyViewController.swift
+++ OneTab/EmptyViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class EmptyViewController: UIViewController {
 
+    @IBOutlet weak var greetingsLabel: UILabel!
+    @IBOutlet weak var newPageButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.greetingsLabel.text = NSLocalizedString("GREETING_INITIAL", comment: "")
+        self.newPageButton.setTitle(NSLocalizedString("NEW_PAGE_INITIAL", comment: ""), forState: .Normal)
+    }
+    
     @IBAction func newTabClicked(sender: AnyObject) {
         let webViewVC = self.storyboard?.instantiateViewControllerWithIdentifier("webViewController") as! WebTabController
         let url = NSURL(string: "https://google.com")!
         webViewVC.baseUrl = url
-        presentViewController(webViewVC, animated: true, completion: nil)
+        presentViewController(webViewVC, animated: true, completion: {
+            self.greetingsLabel.text = NSLocalizedString("GREETING_NORMAL", comment: "")
+            self.newPageButton.setTitle(NSLocalizedString("NEW_PAGE_NORMAL", comment: ""), forState: .Normal)
+        })
     }
 }
