97775293a6535ba44a3509db238928712f3c4aad
Alex Semenov <asemenovboyarka@gmail.com>
2015-12-12 15:42:39 +0200

    Initial commit

---
+++ OneTab/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OneTab
+//
+//  Created by Alexander Semenov on 12/12/15.
+//  Copyright © 2015 X-mass Three. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webViewContainer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
