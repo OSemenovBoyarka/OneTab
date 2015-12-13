9c7a8b6ccd3e7eca4b4f0ed1972d713f763f3b02
Alexander Semenov <asemenovboyarka@gmail.com>
2015-12-13 11:08:47 +0200

    removed useless bridging header and created notification manager

---
+++ OneTab/NotificationManager.swift
@@ -0,0 +1,11 @@
+//
+//  NotificationManager.swift
+//  OneTab
+//
+//  Created by Alexander Semenov on 12/13/15.
+//  Copyright © 2015 X-mass Three. All rights reserved.
+//
+
+class NotificationManager: NSObject {
+
+}
